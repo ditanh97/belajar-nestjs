af5256a6cd48bf82af7974260b97f567454693cf
Dita Nurhalimah <ditanurhalimah@gmail.com>
2023-03-17 00:57:28 +0700

    fix import

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { TodosModule } from './todos/todos.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017/todos'),TodosModule],
+  imports: [MongooseModule.forRoot('mongodb://localhost/todos')],
   controllers: [AppController],
   providers: [AppService],
 })
